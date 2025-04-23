a37952899383e825dbe9e0951c6bb5aab72fcd05
q2l3ntk <zurskij@gmail.com>
2025-04-23 14:28:08 +0300

    Notifications (limited)

---
+++ app/src/main/java/com/example/employee/components/Notification.kt
@@ -0,0 +1,45 @@
+package com.example.employee.components
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES
+import androidx.core.app.NotificationCompat
+import com.example.employee.MainActivity
+import com.example.employee.R
+
+class Notification(private val context: Context) {
+    private val channelId = "my_channel_id"
+    private val chanelName = "My Channel"
+
+    init {
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (SDK_INT >= VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, chanelName, NotificationManager.IMPORTANCE_DEFAULT)
+            val manager = context.getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    fun sendNotification(title: String, message: String) {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val notificationBuilder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(1, notificationBuilder.build())
+    }
+}
