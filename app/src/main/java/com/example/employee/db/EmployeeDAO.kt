3d6c3be3827cba5e7e30abe144ed9d20a69faaee
q2l3ntk <zurskij@gmail.com>
2025-04-23 14:31:48 +0300

    DB update/delete functions

--- app/src/main/java/com/example/employee/db/EmployeeDAO.kt
+++ app/src/main/java/com/example/employee/db/EmployeeDAO.kt
@@ -1,14 +1,22 @@
 package com.example.employee.db
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface EmployeeDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(employee: Employee)
+
+    @Update
+    fun update(employee: Employee)
+
+    @Delete
+    fun delete(employee: Employee)
 
     @Query("SELECT * FROM employees")
     fun getAllEmployees(): List<Employee>
